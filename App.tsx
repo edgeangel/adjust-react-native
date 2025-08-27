37b42d4b3e4b78a2727cf5a831f1b3f02a062d96
Mathieu Lima <mathieu.lima@gmail.com>
2025-08-27 16:52:31 +0200

    Adjust init + iOS att auth wrapper + Google data sharing

--- App.tsx
+++ App.tsx
@@ -1,44 +1,107 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import React, { useEffect } from 'react';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Image, 
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+import { Adjust, AdjustConfig, AdjustThirdPartySharing } from "react-native-adjust";
 
+const App = (): React.JSX.Element => {
+  useEffect(() => {
+    const adjustConfig = new AdjustConfig("{YourAppToken}", 
+    AdjustConfig.EnvironmentSandbox);
+    adjustConfig.setLogLevel(AdjustConfig.LogLevelVerbose);
+
+    // ATT authorization wrapper
+    Adjust.requestAppTrackingAuthorization(function(status) {
+      console.log("Authorization status update!");
+      switch (status) {
+        case 0:
+          // ATTrackingManagerAuthorizationStatusNotDetermined
+          console.log("[Adjust]: Status: Not Determined (l'utilisateur n'a pas encore répondu)");
+          break;
+        case 1:
+          // ATTrackingManagerAuthorizationStatusRestricted
+          console.log("[Adjust]: Status: Restricted (le suivi est bloqué sur l'appareil)");
+          break;
+        case 2:
+          // ATTrackingManagerAuthorizationStatusDenied
+          console.log("[Adjust]: Status: Denied (l'utilisateur a refusé)");
+          break;
+        case 3:
+          // ATTrackingManagerAuthorizationStatusAuthorized
+          console.log("[Adjust]: Status: Authorized (l'utilisateur a accepté)");
+          break;
+      }
+    });
+
+    // Third party sharing 
+    var adjustThirdPartySharing = new AdjustThirdPartySharing(null);  
+    adjustThirdPartySharing.addGranularOption("google_dma", "eea", "1");                // user is in Europe
+    adjustThirdPartySharing.addGranularOption("google_dma", "ad_user_data", "1");       // user consented to Google Ads
+    adjustThirdPartySharing.addGranularOption("google_dma", "ad_personalization", "1"); // user consented to Google Ads
+    adjustThirdPartySharing.addGranularOption("google_dma", "npa", "1");                // user consented to Google Ads
+    Adjust.trackThirdPartySharing(adjustThirdPartySharing);
+
+    Adjust.initSdk(adjustConfig); 
+
+    return () => {
+      Adjust.componentWillUnmount();
+    };
+  }, []);
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <SafeAreaView style={styles.safeArea}>
+      <StatusBar barStyle={'dark-content'}  />
+      <View style={styles.container}>
+        <Text style={styles.title}>Adjust React Native</Text>
+        <View style={styles.logoContainer}>
+          <Image
+            source={require('./src/assets/edgeangel.png')}
+            style={styles.logo}
+          />
+          <Image
+            source={require('./src/assets/react.png')}
+            style={styles.logo}
+          />
+          <Image
+            source={require('./src/assets/adjust.png')}
+            style={styles.logo}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
+};
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+  },
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#333333',
+    marginBottom: 25, 
+  },
+  logoContainer: {
+    flexDirection: 'row', 
+    alignItems: 'center',
+  },
+  logo: {
+    width: 40,
+    height: 40,
+    marginHorizontal: 15, 
   },
 });
 
